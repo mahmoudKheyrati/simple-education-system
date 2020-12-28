3a41bb5802b4b22545240ffa6c4a0a608cf43038
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2020-12-28 10:10:33 +0330

    add get getCollegeLectures to Student

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,23 @@
-class Student{
+import jdk.jshell.spi.ExecutionControl;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+class Student extends Person{
+
+    public Student(String name, ArrayList<College> relatedColleges, ArrayList<String> jobTitles) {
+        super(name, relatedColleges, jobTitles);
+    }
+
+    public ArrayList<Lecture> getCollegeLectures(College college) throws IllegalAccessException {
+        if (!relatedColleges.contains(college)) {
+            throw new IllegalAccessException("you are not a member of this college. so you can't get it's lectures.");
+        }
+        return college.getCollegeLectures();
+    }
+
+    public boolean takeLecture(College college, Lecture lecture) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("not implemented yet!");
+    }
 
 }
